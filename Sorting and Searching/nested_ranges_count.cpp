279ddf20993cce22560f678d7361ebc0b73a6742
Yunus Diler <gmyunusdiler@gmail.com>
2024-05-28 16:33:51 +0300

    nested ranges count solved :D

--- Sorting and Searching/nested_ranges_count.cpp
+++ Sorting and Searching/nested_ranges_count.cpp
@@ -1,5 +1,3 @@
-// NOT A SOLUTION
-
 #include <bits/stdc++.h>
 
 #define int long long
@@ -21,6 +19,36 @@ bool cmpr(pair<pair<int, int>, int> a, pair<pair<int, int>, int> b) {
     return a.first.first > b.first.first;
 }
 
+vector<int> tree;
+int N = 1;
+
+void update(int index)
+{
+    index += N - 1;
+    while (index)
+    {
+        tree[index]++;
+        index >>= 1;
+    } 
+}
+
+int range(int left, int right)
+{
+    left += N - 1;
+    right += N - 1;
+    int sum = 0;
+
+    while (left < right)
+    {
+        if (left & 1) sum -= tree[left ^ 1];
+        left >>= 1;
+        if (!(right & 1)) sum -= tree[right ^ 1];
+        right >>= 1;
+    }
+    sum += tree[right];
+    return sum;
+}
+
 int32_t main() {
     need_for_speed
     
@@ -33,25 +61,60 @@ int32_t main() {
         v[i].first.second = b;
         v[i].second = i;
     }
+
+    vector<int> contains(n);
+    vector<int> contained(n);
+
+    map<int, int> mps;
+    map<int, int> mped;
+
     sort(v.begin(), v.end(), cmp);
-
-    vector<short> contains(n);
-    vector<short> contained(n);
-
-    int m = 0;
     for (auto p : v)
     {
-        if (p.first.second <= m) contained[p.second] = 1;
-        m = max(m, p.first.second);
+        mped[p.first.second] = true;
     }
 
-    sort(v.begin(), v.end(), cmpr);
+    vector<int> fed;
+    int t = 1;
+    for (auto m : mped)
+    {
+        fed.push_back(t);
+        mped[m.first] = t;
+        t++;
+    }
 
-    m = INT_MAX;
+    while (N < fed.size())
+        N <<= 1;
+    tree.resize(2 * N, 0);
+
     for (auto p : v)
     {
-        if (p.first.second >= m) contains[p.second] = 1;
-        m = min(m, p.first.second);
+        contained[p.second] = range(mped[p.first.second], fed.size());
+        update(mped[p.first.second]);
+    }
+
+
+    sort(v.begin(), v.end(), cmpr);
+    for (auto p : v)
+    {
+        mps[p.first.second] = true;
+    }
+
+    vector<int> fs;
+    t = 1;
+    for (auto m : mps)
+    {
+        fs.push_back(t);
+        mps[m.first] = t;
+        t++;
+    }
+
+    for (int i = 0; i < tree.size(); i++) tree[i] = 0;
+
+    for (auto p : v)
+    {
+        contains[p.second] = range(1, mps[p.first.second]);
+        update(mps[p.first.second]);
     }
 
     for (auto b : contains) cout << b << ' ';
