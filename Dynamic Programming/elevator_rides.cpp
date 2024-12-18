88f37ea61140921739f63c10201bea978aa643b3
Yunus Diler <gmyunusdiler@gmail.com>
2024-12-19 02:01:08 +0300

    AC: Elevater Rides

--- Dynamic Programming/elevator_rides.cpp
+++ Dynamic Programming/elevator_rides.cpp
@@ -1,31 +1,43 @@
 #include <bits/stdc++.h>
 
 #define int long long
-#define need_for_speed ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
 
 using namespace std;
 
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
 
 
 int32_t main() {
-    need_for_speed
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     
     int n, x; cin >> n >> x;
-    vector<int> w(n);
-    for (int i = 0; i < n; i++) cin >> w[i];
+    vector<int> v(n);
+    for (auto& i : v) cin >> i;
 
-    vector<pair<int, int>> dp(1 << n);
-    
-    dp[0].first = 1;
-    dp[0].second = 0;
-    for (int i = 0; i < (1 << n); i++)
-    {
-        for (int j = 0; j < 20; j++)
-        {
-            
+    int N = 1 << n;
+    vector<pair<int, int>> vp(N, {INF, x});
+    vp[0] = {1, 0};
+
+    for (int i = 0; i < N; i++) {
+        for (int p = 0; p < n; p++) {
+            if ((1 << p) & i) continue;
+
+            int num = i ^ (1 << p);
+            if (vp[num].first >= vp[i].first) {
+                int a = vp[i].first + (vp[i].second + v[p] > x), b = vp[i].second + v[p];
+                if (b > x) b = v[p];
+                if (a < vp[num].first) vp[num] = {a, b};
+                if (a == vp[num].first) vp[num] = {a, min(b, vp[num].second)};
+            }
         }
     }
+
+    cout << vp[N - 1].first;
     
-    
+
     return 0;
 }
