06b04f86cb5375029c25a4ddb707b1aa0ed0c7b9
Yunus Diler <gmyunusdiler@gmail.com>
2025-01-09 05:41:12 +0300

    CHANGE: multiple occurence trick for ordered set

--- Sorting and Searching/sliding_window_median.cpp
+++ Sorting and Searching/sliding_window_median.cpp
@@ -4,63 +4,7 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-/*** Needs C++11 or C++14 ***/
-
-/// Treap supporting duplicating values in set
-/// Maximum value of treap * ADD must fit in long long
-
-struct Treap{ /// hash = 96814
-    int len;
-    const int ADD = 1000010;
-    const int MAXVAL = 1000000010;
-    unordered_map <long long, int> mp; /// Change to int if only int in treap
-    tree<long long, null_type, less<long long>, rb_tree_tag, tree_order_statistics_node_update> T;
-
-    Treap(){
-        len = 0;
-        T.clear(), mp.clear();
-    }
-
-    inline void clear(){
-        len = 0;
-        T.clear(), mp.clear();
-    }
-
-    inline void insert(long long x){
-        len++, x += MAXVAL;
-        int c = mp[x]++;
-        T.insert((x * ADD) + c);
-    }
-
-    inline void erase(long long x){
-        x += MAXVAL;
-        int c = mp[x];
-        if (c){
-            c--, mp[x]--, len--;
-            T.erase((x * ADD) + c);
-        }
-    }
-
-    /// 1-based index, returns the K'th element in the treap, -1 if none exists
-    inline long long kth(int k){
-        if (k < 1 || k > len) return -1;
-        auto it = T.find_by_order(--k);
-        return ((*it) / ADD) - MAXVAL;
-    }
-
-    /// Count of value < x in treap
-    inline int count(long long x){
-        x += MAXVAL;
-        int c = mp[--x];
-        return (T.order_of_key((x * ADD) + c));
-    }
-
-    /// Number of elements in treap
-    inline int size(){
-        return len;
-    }
-};
-
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 int main()
 {
@@ -68,19 +12,21 @@ int main()
     cin >> n >> k;
     vector<int> v(n);
     for (int i = 0; i < n; i++) cin >> v[i];
-    Treap s;
+    ordered_set s;
     for (int i = 0; i < k; i++) s.insert(v[i]);
-    auto t = s.kth(k / 2 + 1);
-    if (k % 2 == 0) cout << min(t, s.kth(k / 2)) << ' ';
-    else cout << t << ' ';
+    auto p = *s.find_by_order(k / 2);
+    auto q = *s.find_by_order(k / 2 - 1);
+    if (k % 2 == 0) cout << min(p, q) << ' ';
+    else cout << p << ' ';
 
     for (int i = k; i < n; i++)
     {
-        s.erase(v[i - k]);
+        s.erase(s.upper_bound(v[i - k]));
         s.insert(v[i]);
-        auto t = s.kth(k / 2 + 1);
-        if (k % 2 == 0) cout << min(t, s.kth(k / 2)) << ' ';
-        else cout << t << ' ';
+        auto p = *s.find_by_order(k / 2);
+        auto q = *s.find_by_order(k / 2 - 1);
+        if (k % 2 == 0) cout << min(p, q) << ' ';
+        else cout << p << ' ';
     }
 
     return 0;
