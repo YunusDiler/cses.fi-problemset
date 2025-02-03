0810539c11d65c9c8b9e20145fce660d2469e954
Yunus Diler <gmyunusdiler@gmail.com>
2025-02-03 14:56:16 +0300

    AC: Round Trip 2

--- Dynamic Programming/elevator_rides.cpp
+++ Dynamic Programming/elevator_rides.cpp
@@ -8,36 +8,47 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int INF = 1e18;
 
-
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    
-    int n, x; cin >> n >> x;
-    vector<int> v(n);
-    for (auto& i : v) cin >> i;
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-    int N = 1 << n;
-    vector<pair<int, int>> vp(N, {INF, x});
-    vp[0] = {1, 0};
+  int n, x;
+  cin >> n >> x;
+  vector<int> v(n);
+  for (auto& i : v) cin >> i;
 
-    for (int i = 0; i < N; i++) {
-        for (int p = 0; p < n; p++) {
-            if ((1 << p) & i) continue;
+  int N = 1 << n;
+  vector<int> rides(N, INF);
+  vector<int> weight(N, x);
+  rides[0] = 1;
+  weight[0] = 0;
 
-            int num = i ^ (1 << p);
-            if (vp[num].first >= vp[i].first) {
-                int a = vp[i].first + (vp[i].second + v[p] > x), b = vp[i].second + v[p];
-                if (b > x) b = v[p];
-                if (a < vp[num].first) vp[num] = {a, b};
-                if (a == vp[num].first) vp[num] = {a, min(b, vp[num].second)};
-            }
+  for (int i = 0; i < N; i++) {
+    for (int p = 0; p < n; p++) {
+      if ((1 << p) & i) continue;
+
+      int num = i | (1 << p);
+      if (rides[num] >= rides[i]) {
+        int a = rides[i];
+        int b = weight[i] + v[p];
+
+        if (b > x) {
+          b = v[p];
+          a++;
         }
+
+        if (a < rides[num]) {
+          rides[num] = a;
+          weight[num] = b;
+        }
+
+        if (a == rides[num]) weight[num] = min(b, weight[num]);
+      }
     }
+  }
 
-    cout << vp[N - 1].first;
-    
+  cout << rides[N - 1];
 
-    return 0;
+  return 0;
 }
--- Graph Algorithms/round_trip_2.cpp
+++ Graph Algorithms/round_trip_2.cpp
@@ -5,60 +5,62 @@
 
 using namespace std;
 
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+const int MAXN = 1e5 + 1;
+
+vector<vector<int>> adj(MAXN);
+vector<int> color(MAXN);
+vector<int> parent(MAXN);
+int cycle_start = 0, cycle_end = 0;
+
+void dfs(int u) {
+  if (cycle_end) return;
+  color[u] = 1;
+  for (auto v : adj[u]) {
+    if (cycle_end) return;
+    if (color[v] == 0) {
+      parent[v] = u;
+      dfs(v);
+    }
+    else if (color[v] == 1) {
+      //cycle
+      cycle_end = u;
+      cycle_start = v;
+      return;
+    }
+  }
+  color[u] = 2;
+}
+
 int32_t main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    
-    int n, m; cin >> n >> m;
-    vector<vector<int>> adj(n + 1);
-    for (int i = 0; i < m; i++)
-    {
-       int u, v; cin >> u >> v;
-       adj[u].push_back(v);
-    }
-    
-    vector<int> vis(n + 1);
-    vector<int> parent(n + 1);
 
-    stack<int> s;
-    
-    int count = 0;
-    for (int i = 1; i <= n; i++) {
-        if (!vis[i]) {
-            s.push(i);
-            count++;
-        }
-
-        while (!s.empty()) {
-            int from = s.top();
-            s.pop();
-
-            vis[from] = count;
-            for (auto to : adj[from]) {
-                if (vis[to] == count) {
-                    parent[to] = from;
-                    deque<int> ans;
-                    ans.push_back(from);
-                    int u = parent[from];
-                    while (u != from) {
-                        ans.push_front(u);
-                        u = parent[u];
-                    }
-                    cout << ans.size() + 1 << endl;
-                    cout << from << ' ';
-                    for (auto& i : ans) cout << i << ' ';
-                    return 0;
-                }
-                else {
-                    s.push(to);
-                    parent[to] = from;
-                }
-            }
-        }
+    int N, M; cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+      int a, b; cin >> a >> b;
+      adj[a].push_back(b);
     }
 
-    cout << "IMPOSSIBLE";
+    for (int i = 1; i <= N; i++) {
+      if (!color[i]) dfs(i);
+      if (cycle_end) break;
+    }
+
+    if (cycle_end == 0) {cout << "IMPOSSIBLE"; return 0;}
+
+    vector<int> ans = {cycle_start};
+    while (cycle_end != cycle_start) {
+      ans.push_back(cycle_end);
+      cycle_end = parent[cycle_end];
+    }
+    ans.push_back(cycle_start);
+    reverse(ans.begin(), ans.end());
+
+    cout << ans.size() << endl;
+    for (auto i : ans) cout << i << ' ';
     
     return 0;
 }
